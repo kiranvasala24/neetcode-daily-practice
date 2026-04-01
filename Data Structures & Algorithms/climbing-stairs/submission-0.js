79244122b2e8bc339d68f614415326775a2b84cf
Kirankumar Vasala <87659247+kiranvasala24@users.noreply.github.com>
2026-04-01 12:55:51 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        if (n === 1){ return 1;}
+        if (n === 2) { return 2;}
+
+        let prev2 = 1;
+        let prev1 = 2;
+
+        for (let i = 3; i <= n; i++){
+            let current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
